df1f096700d6460125075f76739d4cd78fd4bb68
Alistair Smith <alistairsmith01@gmail.com>
2021-04-10 20:33:23 +0100

    prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: ["./src/**/*.{ts,tsx}"],
-  darkMode: "media",
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	purge: ["./src/**/*.{ts,tsx}"],
+	darkMode: "media",
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
