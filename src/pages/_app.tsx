e29c7e68e0465740083be230286ec92a222fb46b
Alistair Smith <alistairsmith01@gmail.com>
2021-04-10 21:11:22 +0100

    fix: Page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import {AppProps} from "next/app";
+import Head from "next/head";
 
 import "tailwindcss/tailwind.css";
 import "../styles/main.css";
 
 export default function App({Component, pageProps}: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Discord List List</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
