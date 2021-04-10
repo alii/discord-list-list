86bdf2d41e4a82e18b87f39bb3fc336045856d0d
Alistair Smith <alistairsmith01@gmail.com>
2021-04-10 20:45:17 +0100

    Basic Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,19 @@ const categories = Object.entries(data.categories);
 
 export default function Home() {
 	return (
-		<div className="max-w-7xl mx-auto">
-			{categories.map((entry) => {
-				const [category, items] = entry;
-				return <div key={category}>{JSON.stringify(items)}</div>;
-			})}
-		</div>
+		<>
+			<div className="shadow-md bg-gray-100 dark:bg-gray-800">
+				<div className="py-36 max-w-4xl mx-auto">
+					<h1 className="font-bold text-4xl">Discord List List</h1>
+				</div>
+			</div>
+
+			<div className="max-w-4xl mx-auto mt-8">
+				{categories.map((entry) => {
+					const [category, items] = entry;
+					return <div key={category}>{JSON.stringify(items)}</div>;
+				})}
+			</div>
+		</>
 	);
 }
