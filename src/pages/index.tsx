9eaeb80210753ca8c0306ce9cff5d0ae83f87b1b
Alistair Smith <alistairsmith01@gmail.com>
2021-04-10 20:37:32 +0100

    Loop through items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import data from "../data.json";
+
+const categories = Object.entries(data.categories);
+
 export default function Home() {
-	return <div className="max-w-7xl mx-auto">Hello, Tailwind!</div>;
+	return (
+		<div className="max-w-7xl mx-auto">
+			{categories.map((entry) => {
+				const [category, items] = entry;
+				return <div key={category}>{JSON.stringify(items)}</div>;
+			})}
+		</div>
+	);
 }
