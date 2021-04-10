cc8cf43716661d78a3c26de1aad12f8ca4061f48
Alistair Smith <alistairsmith01@gmail.com>
2021-04-10 21:08:38 +0100

    feat: Card hover effect

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,7 +4,10 @@ type CardProps = typeof data["categories"]["Servers"][number];
 
 export function Card(props: CardProps) {
 	return (
-		<a href={props.link} className="block rounded-md bg-gray-700 p-4 bg-opacity-20">
+		<a
+			href={props.link}
+			className="block rounded-md bg-gray-700 p-4 bg-opacity-5 dark:bg-opacity-20 hover:bg-opacity-10"
+		>
 			<h2 className="text-2xl font-bold">{props.name}</h2>
 			<p>{props.description}</p>
 		</a>
