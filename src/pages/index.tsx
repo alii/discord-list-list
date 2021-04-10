16d5c534be24eac9f8fd72f3bb897f23b1368065
Alistair Smith <alistairsmith01@gmail.com>
2021-04-10 20:50:38 +0100

    yea

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,13 @@ export default function Home() {
 	return (
 		<>
 			<div className="shadow-md bg-gray-100 dark:bg-gray-800">
-				<div className="py-36 max-w-4xl mx-auto">
+				<div className="py-36 max-w-4xl mx-auto space-y-2 px-4">
 					<h1 className="font-bold text-4xl">Discord List List</h1>
+					<input
+						type="text"
+						placeholder="Search"
+						className="bg-gray-900 bg-opacity-30 px-4 py-2 rounded-md w-full md:w-1/2 outline-none focus:outline-none focus:ring ring-gray-300"
+					/>
 				</div>
 			</div>
 
