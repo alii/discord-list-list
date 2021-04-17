1c267610da53dcdc4f61956845920144c50082cc
EGGSY <eggsydev@outlook.com>
2021-04-17 21:59:47 +0300

    chore: use div

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -16,13 +16,13 @@ export function Card(props: CardProps) {
 		);
 	else
 		return (
-			<button
+			<div
 				onClick={() => window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")}
-				className="block p-4 space-y-2 text-left transition duration-200 ease-in-out bg-gray-700 rounded-md bg-opacity-5 dark:bg-opacity-20 hover:bg-opacity-10 hover:text-white hover:bottom-1 focus:outline-none"
+				className="block p-4 space-y-2 text-left transition duration-200 ease-in-out bg-gray-700 rounded-md cursor-pointer bg-opacity-5 dark:bg-opacity-20 hover:bg-opacity-10 hover:text-white hover:bottom-1 focus:outline-none"
 			>
 				<h2 className="text-2xl font-bold line-through">{props.name}</h2>
 				<p className="line-through opacity-50 dark:opacity-75">{props.description}</p>
 				<small className="text-sm text-red-700 uppercase">Will be removed soon</small>
-			</button>
+			</div>
 		);
 }
