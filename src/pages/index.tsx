c6eb8af6ff5db64fed7efcfb2cc541c17e292b4b
EGGSY <eggsydev@outlook.com>
2021-04-18 05:06:29 +0300

    chore: improve code and page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,31 +12,28 @@ export default function Home() {
 
 	return (
 		<>
-			<div className="shadow-md bg-gray-100 dark:bg-gray-800">
-				<div className="max-w-4xl mx-3 md:mx-auto pt-5">
+			<header className="py-10 bg-gray-100 shadow-md dark:bg-gray-800">
+				<div className="container px-4 mx-auto space-y-4 md:px-0 md:max-w-4xl">
 					<img
-						src="https://static.vecteezy.com/system/resources/previews/001/191/986/non_2x/circle-logo-png.png"
-						className="monotonize-image"
+						src="/logo.webp"
+						className="mx-auto md:mx-0 brightness-0 dark:invert filter"
 						alt="Discord"
 						height={64}
 						width={64}
 					/>
-				</div>
 
-				<div className="mx-3 content-center py-24 max-w-4xl md:mx-auto md:space-y-4">
-					<h1 className="mb-5 text-center font-bold text-4xl md:text-left dark:text-white">
+					<h1 className="text-4xl font-bold text-center text-gray-900 md:text-left dark:text-gray-100">
 						Discord List List
 					</h1>
 
 					<input
 						type="text"
 						placeholder="Search"
-						className="bg-gray-900 placeholder-gray-200 dark:placeholder-gray-600 bg-opacity-30 px-4 py-2 rounded-md w-full md:w-1/2 outline-none focus:outline-none focus:ring ring-gray-300"
+						className="w-full px-4 py-2 placeholder-gray-500 bg-gray-300 rounded-md outline-none dark:bg-gray-900 dark:placeholder-gray-400 bg-opacity-30 md:w-1/2 focus:outline-none focus:shadow-sm"
 						onChange={(e) => setState(e.target.value)}
-						value={state}
 					/>
 
-					<p className="my-3 text-sm md:text-md text-justify opacity-50 md:w-1/2 dark:text-white">
+					<p className="text-sm text-justify opacity-50 md:text-md md:w-1/2 dark:text-white">
 						If you dont use Discord List List to find the best Discord list, then how do you know that you
 						have the best discord list from the potential Discord lists to find the best Discord? Look no
 						more, for here is Discord List List.
@@ -45,18 +42,19 @@ export default function Home() {
 					<a
 						href="https://www.producthunt.com/posts/discord-list-list?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-discord-list-list"
 						target="_blank"
-						className="flex justify-center mt-10 md:inline-block"
+						rel="noreferrer"
+						className="flex justify-center md:inline-block"
 					>
 						<img
 							src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=291721&theme=light"
 							alt="Discord List List - The ultimate list for finding the perfect Discord list. | Product Hunt"
 							width="200"
 							height="54"
-							className="block"
+							className="block transition filter dark:md:grayscale dark:hover:grayscale-0"
 						/>
 					</a>
 				</div>
-			</div>
+			</header>
 
 			<div className="max-w-4xl mx-3 mt-8 md:mx-auto">
 				{state === "" ? (
@@ -65,10 +63,10 @@ export default function Home() {
 							const [category, items] = entry;
 							return (
 								<div key={category}>
-									<h2 className="text-center text-4xl font-bold mb-4 mt-10 md:text-left">
+									<h2 className="mt-10 mb-4 text-4xl font-semibold text-center md:text-left">
 										{category}
 									</h2>
-									<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+									<div className="grid grid-cols-1 gap-4 md:grid-cols-3">
 										{items.map((item) => {
 											return (
 												<Card
@@ -86,7 +84,7 @@ export default function Home() {
 					</>
 				) : (
 					<div className="max-w-4xl mx-auto mt-8">
-						<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+						<div className="grid grid-cols-1 gap-4 md:grid-cols-3">
 							{filtered.map((item) => {
 								return (
 									<Card
@@ -98,12 +96,11 @@ export default function Home() {
 								);
 							})}
 						</div>
+
 						{!filtered.length && <p className="text-center">No results :(</p>}
 					</div>
 				)}
-				<br />
-				<p className={"text-center text-gray-600"}>Not affiliated with Discord</p>
-				<br />
+				<p className={"text-center text-gray-600 py-8"}>Not affiliated with Discord Inc.</p>
 			</div>
 		</>
 	);
